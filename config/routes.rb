6cedd56fc4ed2173d7cc8d2c2c5260a25ea02d78
Roman <roman@MacBook-Pro-Roman-2.local>
2017-10-10 15:04:00 +0300

    validate Order

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
 
   get 'store/index'
 
+  resources :products do
+
+    get :who_bought, :on => :member
+
+  end
+  
   resources :products
   resource :example, only: [:show], controller: :example
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
